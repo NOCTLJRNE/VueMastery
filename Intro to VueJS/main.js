f1f8eab95725f934aaf002631eedffe4c7723ae1
NOCTLJRNE <baoan1602@gmail.com>
2019-06-09 01:03:54 +0200

    Event Handling

--- Intro to VueJS/main.js
+++ Intro to VueJS/main.js
@@ -6,6 +6,18 @@ var app = new Vue({
     inventory: 5,
     isShow: true,
     details: ["80% cotton", "20% polyester", "gender-neutral"],
-    variants: [{ id: 2234, color: "green" }, { id: 2235, color: "blue" }]
+    variants: [
+      { id: 2234, color: "green", image: "./assets/vmSocks-green-onWhite.jpg" },
+      { id: 2235, color: "blue", image: "./assets/vmSocks-blue-onWhite.jpg" }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart: function() {
+      this.cart += 1;
+    },
+    updateProduct: function(variantImage) {
+      this.image = variantImage;
+    }
   }
 });
