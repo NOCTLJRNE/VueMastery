c35ca9460f812cb95cd556c7b69bc71d562ca9a7
NOCTLJRNE <baoan1602@gmail.com>
2019-06-09 18:24:20 +0200

    Class & Style Binding

--- Intro to VueJS/main.js
+++ Intro to VueJS/main.js
@@ -3,6 +3,7 @@ var app = new Vue({
   data: {
     product: "Socks",
     image: "./assets/vmSocks-green-onWhite.jpg",
+    inStock: false,
     inventory: 5,
     isShow: true,
     details: ["80% cotton", "20% polyester", "gender-neutral"],
