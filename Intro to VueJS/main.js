096a6ccf184dd1ecab73dca86424e17a09994131
NOCTLJRNE <baoan1602@gmail.com>
2019-06-12 19:44:14 +0200

    Forms

--- Intro to VueJS/main.js
+++ Intro to VueJS/main.js
@@ -46,6 +46,19 @@ Vue.component("product", {
     </button>
     <button v-on:click="removeFromCart" v-bind:disabled="!cart" :class="[cart ? '' : 'disabledButton', 'errorClass']"> Remove from cart</button>
   </div>
+  <div>
+  <h2> Reviews </h2>
+    <p v-if="!reviews.length"> There are no reviews yet. </p>
+      <ul>
+        <li v-for="review in reviews"> 
+        <p>{{ review.name }}</p>
+        <p>Rating: {{ review.rating }}</p>
+        <p>{{ review.review }}</p>
+        </li>
+      </ul>
+   
+  </div>
+  <product-review @review-submitted-event="addReview"></product-review>
 </div>`,
   data() {
     return {
@@ -70,7 +83,8 @@ Vue.component("product", {
           image: "./assets/vmSocks-blue-onWhite.jpg",
           quantity: 0
         }
-      ]
+      ],
+      reviews: []
     };
   },
   methods: {
@@ -84,6 +98,9 @@ Vue.component("product", {
     removeFromCart() {
       console.log("remove");
       this.$emit("remove-from-cart-event");
+    },
+    addReview(productReview) {
+      this.reviews.push(productReview);
     }
   },
   computed: {
@@ -98,6 +115,65 @@ Vue.component("product", {
     },
     shipping: function() {
       return this.premium ? "Free" : "$3.99";
+    }
+  }
+});
+Vue.component("product-review", {
+  template: `
+  <form class="review-form" @submit.prevent="onSubmit">
+  <p v-if="errors.length">
+  <b>Please correct the following error(s)</b>
+  <ul>
+    <li v-for="error in errors">{{error}}</li>
+  </ul>
+  </p>
+  <p>
+    <label for="name">Name;</label>
+    <input id="name" v-model="name">
+  </p>
+  <p>
+    <label for="review">Review;</label>
+    <textarea id="review" v-model="review" required></textarea> 
+  </p>
+  <label for="rating">Rating:</label>
+  <select id="rating" v-model.number="rating">
+    <option>5</option>  
+    <option>4</option>
+    <option>3</option>
+    <option>2</option>
+    <option>1</option>  
+  </select>
+  </p>
+<p>
+<input type="submit" value="Submit">
+</p>
+  </form>
+  `,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null,
+      errors: []
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (this.name && this.review && this.rating) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating
+        };
+        this.$emit("review-submitted-event", productReview);
+        this.name = null;
+        this.review = null;
+        this.rating = null;
+      } else {
+        if (!this.name) this.errors.push("Name required");
+        if (!this.review) this.errors.push("Review required");
+        if (!this.rating) this.errors.push("Rating required");
+      }
     }
   }
 });
@@ -118,3 +194,4 @@ var app = new Vue({
     }
   }
 });
+Vue.config.devtools = true;
